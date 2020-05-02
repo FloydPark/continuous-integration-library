0266299c76a1a7d17c705cc1ebe2ad458f349099
nancho313 <nancho313@hotmail.com>
2020-05-02 12:35:44 -0500

    add angular pipeline stages configuration

--- vars/angularCIPipeline.groovy
+++ vars/angularCIPipeline.groovy
@@ -5,7 +5,7 @@ def call() {
         stages {
             stage('Build') {
                 steps {
-                    echo 'In progress...'
+                    sh 'node -v'
                 }
             }
             stage('Test') {
