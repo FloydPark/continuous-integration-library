4661bfe9ceb539b293db3fd933922ff9b1745521
nancho313 <nancho313@hotmail.com>
2020-05-03 22:00:45 -0500

    use SonarQubeUtil in sonarqubeGradle pipeline

--- vars/sonarqubeGradle.groovy
+++ vars/sonarqubeGradle.groovy
@@ -1,27 +1,10 @@
+import com.floydpark.devops.ci.SonarQubeUtil
+
 def call(env){
+
+    def sonarQubeUtil = new SonarQubeUtil()
+
     withSonarQubeEnv("SonarCloud") {
-        sh './gradlew sonarqube '+getProperties(env)
+        sh './gradlew sonarqube '+sonarQubeUtil.getProperties(env)
     }
-}
-
-private String getProperties(env){
-
-    String properties = "";
-    def branch = env.BRANCH_NAME;
-    println(branch)
-
-    if(branch.equals('develop') || branch.equals("master") || branch.startsWith("feature")){
-
-        properties = "-Dsonar.branch.name=$branch"
-    }
-    else
-        if(branch.startsWith('PR-')){
-            def key = branch.split("-")[1]
-            branch = env.CHANGE_BRANCH
-            properties = "-Dsonar.pullrequest.base=develop"
-            properties = "$properties -Dsonar.pullrequest.branch=$branch"
-            properties = "$properties -Dsonar.pullrequest.key=$key"
-        }
-
-    return properties;
 }
